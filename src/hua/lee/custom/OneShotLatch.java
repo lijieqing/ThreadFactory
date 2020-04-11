b2d72cf38ae2e1ea19b1189cc57ee0ee15362e0d
lijie <lijie@formovie.cn>
2020-04-11 18:08:29 +0800

    增加 AQS 简单实现类 OneShotLatch

--- src/hua/lee/custom/OneShotLatch.java
+++ src/hua/lee/custom/OneShotLatch.java
@@ -4,19 +4,21 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class OneShotLatch {
     private final Sync sync = new Sync();
-    public void signal(){
+
+    public void signal() {
         sync.releaseShared(0);
     }
 
-    public void await() throws InterruptedException{
+    public void await() throws InterruptedException {
         sync.acquireSharedInterruptibly(0);
     }
-    private class Sync extends AbstractQueuedSynchronizer{
-        private static final long serialVersionUID = 1229612274290488163L;
 
+    private static class Sync extends AbstractQueuedSynchronizer {
         @Override
         protected int tryAcquireShared(int arg) {
-            return getState()==1 ? 1 : -1;
+            int state = getState();
+            System.out.println("state = " + state);
+            return getState() == 1 ? 1 : -1;
         }
 
         @Override
@@ -24,6 +26,49 @@ public class OneShotLatch {
             setState(1);
             return true;
         }
+    }
 
+    public static void main(String[] args) {
+        OneShotLatch osl = new OneShotLatch();
+        new Thread(() -> {
+            System.out.println("we are in main 01 thread, and start osl await");
+            try {
+                osl.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("main 01 thread osl await finished");
+        }).start();
+        new Thread(() -> {
+            System.out.println("we are in main 02 thread, and start osl await");
+            try {
+                osl.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("main 02 thread osl await finished");
+        }).start();
+        new Thread(() -> {
+
+            System.out.println("we are in main 03 thread, and first sleep 5s");
+            try {
+                Thread.sleep(5000);
+                System.out.println("we are in main 03 thread, and start osl await");
+                osl.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("main 03 thread osl await finished");
+        }).start();
+        new Thread(() -> {
+            System.out.println("we are in work thread,and we start waiting");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("work finish,now we signal main thread");
+            osl.signal();
+        }).start();
     }
 }
--- src/hua/lee/thread/Level.java
+++ src/hua/lee/thread/Level.java
@@ -1,7 +1,39 @@
 package hua.lee.thread;
-public final class Level{
-    private Level(){}
+
+public final class Level {
+    private Level() {
+    }
 
     public static final int SERVER = 0;
     public static final int FINE = 1;
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                while (a < 8) {
+                    System.out.println("A before yield " +  a);
+                    Thread.yield();
+                    System.out.println("A after yield " + a);
+                    a++;
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("B before yield");
+                Thread.yield();
+                System.out.println("B after yield");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("B after sleep");
+            }
+        }).start();
+    }
 }
