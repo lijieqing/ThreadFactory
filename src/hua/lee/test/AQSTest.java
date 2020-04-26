387838072e6f7573fa3bc09b37b5ecef5b3f93ed
lijie <lijie@formovie.cn>
2020-04-26 14:15:48 +0800

    test

--- src/hua/lee/test/AQSTest.java
+++ src/hua/lee/test/AQSTest.java
@@ -1,8 +1,6 @@
 package hua.lee.test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -12,5 +10,33 @@ public class AQSTest {
     private Semaphore semaphore;
     private CountDownLatch countDownLatch;
     private ReentrantReadWriteLock reentrantReadWriteLock;
+
+    private static class MyCallaback implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            System.out.println("=========1=========");
+            Thread.sleep(5000);
+            System.out.println("=========2=========");
+            System.out.println("=========3=========");
+            return "finish";
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(11);
+        Future<String> task = executor.submit(new MyCallaback());
+        String res;
+        try {
+            res = task.get(2, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+            task.cancel(true);
+            res = "false";
+        }
+        System.out.println("first task res = "+res);
+        Thread.sleep(1000);
+        task = executor.submit(new MyCallaback());
+        System.out.println("second task res = "+task.get());
+    }
 
 }
