e0416e60d35cbcc170787047510099c1e87ebba2
lijieqing <759353118@qq.com>
2020-05-16 20:32:13 +0800

    add jvm package

---
+++ src/hua/lee/jvm/MethodTest.java
@@ -0,0 +1,14 @@
+package hua.lee.jvm;
+
+public class MethodTest {
+    public static void main(String[] args) {
+        Lava lava = new Lava();
+        lava.flow();
+    }
+    
+}
+
+class Lava{
+    private int speed = 5;
+    void flow(){}
+}
