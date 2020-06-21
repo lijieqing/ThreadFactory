68f7b4571fd3587b8a86f6c494e5b2b86d49a630
lijie <lijie@formovie.cn>
2020-06-21 21:35:29 +0800

    jvm

--- src/hua/lee/jvm/Angry.java
+++ src/hua/lee/jvm/Angry.java
@@ -17,10 +17,50 @@ class Dog{
 
 class Example{
     public static void main(String[] args) {
-        System.out.println(Angry.angerLevel);
-        System.out.println(Dog.greeting);
+        if (args.length != 2){
+            System.out.println("illegal args");
+            return;
+        }
+        //args[0] + args[1]
+        //编译器会创建StringBuilder
+        //通过 append 连接
+        //并通过 toString 转成 String 对象
+        System.out.println(args[0] + args[1]);
     }
     static{
         System.out.println("Example was initialized");
     }
+}
+
+class ExampleCons extends Example{
+    private int width = 3;
+
+    public ExampleCons() {
+        this(1);
+        System.out.println("ExampleCons(),width = " + width);
+    }
+
+    public ExampleCons(int width) {
+        this.width = width;
+        System.out.println("ExampleCons(int),width = " + width);
+    }
+    public ExampleCons(String msg) {
+        super();
+        System.out.println("ExampleCons(String),width = " + width);
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+        String msg = "Test Constructor MSG";
+        ExampleCons one = new ExampleCons();
+        ExampleCons two = new ExampleCons(2);
+        ExampleCons three = new ExampleCons(msg);
+    }
+}
+
+class MyThread extends Thread implements Cloneable{
+}
+
+class StaticTest{
+    static int len = 9;
 }
