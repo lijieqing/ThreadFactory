bb8d273eb76277a5ee0e3457374f28dcdfbec139
lijie <lijie@formovie.cn>
2020-06-24 18:13:33 +0800

    增加静态变量初始化测试代码

--- src/hua/lee/jvm/InitExample.java
+++ src/hua/lee/jvm/InitExample.java
@@ -3,22 +3,34 @@ package hua.lee.jvm;
 public class InitExample {
 }
 
-class ExampleA{
-    public static int random(){
+class ExampleA {
+    public static int random() {
         System.out.println("A random called");
-        return (int) (Math.random()*10);
+        return (int) (Math.random() * 10);
     }
 }
-class ExampleB{
+
+class ExampleB {
     static final int angle = 10;
     static final int length = angle * 2;
 }
 
-class ExampleC{
+class ExampleC {
     static int a = ExampleI.ketch;
     static int b = ExampleI.mus;
 }
-interface ExampleI{
+
+interface ExampleI {
     int ketch = 9;
-    int mus = (int) (Math.random()*10);
+    int mus = (int) (Math.random() * 10);
+}
+
+class ExampleD {
+    public static void main(String[] args) {
+        String a = "ABC";
+        String b = "A" + "B" + "C";
+        String c = new String("ABC");
+        StringBuilder sb = new StringBuilder();
+        sb.append("A").append("B").append("C");
+    }
 }
